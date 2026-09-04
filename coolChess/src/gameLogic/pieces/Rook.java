38cbc38952548ba79afa92861fa4dedf6fd000bf
cfawcet0306 <colinfawcett1@gmail.com>
2026-09-03 22:47:32 -0400

    Implemented Rook movement
    Need to add more tests and documentation for it.

--- coolChess/src/gameLogic/pieces/Rook.java
+++ coolChess/src/gameLogic/pieces/Rook.java
@@ -1,5 +1,6 @@
 package gameLogic.pieces;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import gameLogic.ChessBoard;
@@ -15,10 +16,62 @@ public class Rook extends Piece {
 	
 	@Override
 	public List<Tile> getValidMoves(ChessBoard board) {
-		// TODO Auto-generated method stub
-		return null;
+		List<Tile> validMoves = new ArrayList<Tile>();
+		
+		getNorth(validMoves, this.getPosition(), board);
+		getEast(validMoves, this.getPosition(), board);
+		getSouth(validMoves, this.getPosition(), board);
+		getWest(validMoves, this.getPosition(), board);
+		
+		return validMoves;
 	}
 
+	private void getNorth(List<Tile> validMoves, Tile currentTile, ChessBoard board) {
+		if(board.getNorth(currentTile) == null) {
+			return;
+		}
+		if(board.getNorth(currentTile).getPiece() == null 
+				|| board.getNorth(currentTile).getPiece().getColor() != this.getColor()) {
+			validMoves.add(board.getNorth(currentTile));
+			this.getNorth(validMoves, board.getNorth(currentTile), board);
+		}
+	}
+	
+	private void getEast(List<Tile> validMoves, Tile currentTile, ChessBoard board) {
+		if(board.getEast(currentTile) == null) {
+			return;
+		}
+		if(board.getEast(currentTile).getPiece() == null 
+				|| board.getEast(currentTile).getPiece().getColor() != this.getColor()) {
+			validMoves.add(board.getEast(currentTile));
+			this.getEast(validMoves, board.getEast(currentTile), board);
+		}
+	}
+	
+	private void getSouth(List<Tile> validMoves, Tile currentTile, ChessBoard board) {
+		if(board.getSouth(currentTile) == null) {
+			return;
+		}
+		if(board.getSouth(currentTile).getPiece() == null 
+				|| board.getSouth(currentTile).getPiece().getColor() != this.getColor()) {
+			validMoves.add(board.getSouth(currentTile));
+			this.getSouth(validMoves, board.getSouth(currentTile), board);
+		}
+	}
+	
+	private void getWest(List<Tile> validMoves, Tile currentTile, ChessBoard board) {
+		if(board.getWest(currentTile) == null) {
+			return;
+		}
+		if(board.getWest(currentTile).getPiece() == null 
+				|| board.getWest(currentTile).getPiece().getColor() != this.getColor()) {
+			validMoves.add(board.getWest(currentTile));
+			this.getWest(validMoves, board.getWest(currentTile), board);
+		}
+	}
+	
+
+	
 	
 
 	
--- coolChess/test/gameLogic/pieces/RookTest.java
+++ coolChess/test/gameLogic/pieces/RookTest.java
@@ -7,7 +7,6 @@ import java.util.List;
 import org.junit.jupiter.api.Test;
 
 import gameLogic.ChessBoard;
-import gameLogic.ChessBoardLogic;
 import gameLogic.Piece;
 import gameLogic.Tile;
 
